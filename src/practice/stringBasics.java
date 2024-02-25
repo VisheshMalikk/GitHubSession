c95b390cccb38bbea26fa2b6947a7991a228364c
Vishesh Malik <Visheshmalikk@gmail.com>
2024-02-25 10:43:25 +0530

    Vishesh Malik : First commit

---
+++ src/practice/stringBasics.java
@@ -0,0 +1,13 @@
+package practice;
+
+import java.text.SimpleDateFormat;
+
+public class stringBasics {
+
+	public static void main(String[] args) {
+		
+		String timeStamp = new SimpleDateFormat("MM/dd/yyy KK:mm a").format(new java.util.Date());
+		System.out.println(timeStamp);
+	}
+
+}
