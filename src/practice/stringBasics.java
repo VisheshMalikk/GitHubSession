a4341cc9b6e6f05c6ddd2a5b80b34acd686edd8e
Vishesh Malik <Visheshmalikk@gmail.com>
2024-02-25 10:57:53 +0530

    Contains stars

--- src/practice/stringBasics.java
+++ src/practice/stringBasics.java
@@ -9,6 +9,7 @@ public class stringBasics {
 		String timeStamp = new SimpleDateFormat("MM/dd/yyy KK:mm a").format(new java.util.Date());
 		System.out.println(timeStamp);
 		System.out.println("Vishesh Malik");
+		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 	}
 
 }
